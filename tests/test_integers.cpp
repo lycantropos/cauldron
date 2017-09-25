9fe5c2edd0acad892731853e4c5b8daccf5814de
Azat Ibrakov <azatibrakov@gmail.com>
2017-09-25 14:43:47 +0700

    Extracted strategy range validation into separate section

--- tests/test_integers.cpp
+++ tests/test_integers.cpp
@@ -4,49 +4,63 @@
 
 
 template<typename T>
-bool is_strategy_valid() {
-  std::random_device random_device;
-  auto distribution = std::uniform_int_distribution<T>();
-  std::vector<T> borders{distribution(random_device),
-                         distribution(random_device)};
-  auto min_value = *std::min_element(borders.begin(), borders.end());
-  auto max_value = *std::max_element(borders.begin(), borders.end());
-  strategies::Integers<T> integers(min_value, max_value);
-
+bool strategy_stays_in_range(const strategies::Integers<T> &integers,
+                             T min_value,
+                             T max_value) {
   auto integer = integers();
   return min_value <= integer <= max_value;
 }
 
 
+template<typename T>
+void validate_strategy() {
+  std::random_device random_device;
+  auto distribution = std::uniform_int_distribution<T>();
+  std::vector<T> borders{distribution(random_device),
+                         distribution(random_device)};
+  auto min_value = *std::min_element(borders.begin(),
+                                     borders.end());
+  auto max_value = *std::max_element(borders.begin(),
+                                     borders.end());
+  strategies::Integers<T> integers(min_value,
+                                   max_value);
+  SECTION("stays in range") {
+    REQUIRE(strategy_stays_in_range<T>(integers,
+                                       min_value,
+                                       max_value));
+  }
+}
+
+
 TEST_CASE("\"integers\" strategy", "[integers]") {
   SECTION("unsigned char") {
-    REQUIRE(is_strategy_valid<unsigned char>());
+    validate_strategy<unsigned char>();
   }
   SECTION("signed char") {
-    REQUIRE(is_strategy_valid<signed char>());
+    validate_strategy<signed char>();
   }
   SECTION("short int") {
-    REQUIRE(is_strategy_valid<short>());
+    validate_strategy<short>();
   }
   SECTION("unsigned short int") {
-    REQUIRE(is_strategy_valid<unsigned short>());
+    validate_strategy<unsigned short>();
   }
   SECTION("int") {
-    REQUIRE(is_strategy_valid<int>());
+    validate_strategy<int>();
   }
   SECTION("unsigned int") {
-    REQUIRE(is_strategy_valid<unsigned>());
+    validate_strategy<unsigned>();
   }
   SECTION("long int") {
-    REQUIRE(is_strategy_valid<long>());
+    validate_strategy<long>();
   }
   SECTION("unsigned long int") {
-    REQUIRE(is_strategy_valid<unsigned long>());
+    validate_strategy<unsigned long>();
   }
   SECTION("long long int") {
-    REQUIRE(is_strategy_valid<long long>());
+    validate_strategy<long long>();
   }
   SECTION("unsigned long long int") {
-    REQUIRE(is_strategy_valid<unsigned long long>());
+    validate_strategy<unsigned long long>();
   }
 }
