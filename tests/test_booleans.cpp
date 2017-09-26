1824efff084f7cce6cc87ef81cdb6ea522c7ac4f
Azat Ibrakov <azatibrakov@gmail.com>
2017-09-26 14:21:50 +0700

    Added tests for 'Booleans' filtration

--- tests/test_booleans.cpp
+++ tests/test_booleans.cpp
@@ -11,4 +11,36 @@ TEST_CASE("\"booleans\" strategy", "[booleans]") {
     REQUIRE(not false_values());
     REQUIRE(true_values());
   }
+  SECTION("filtration") {
+    auto identity = [](bool boolean) -> bool {
+      return boolean;
+    };
+    auto negate = [](bool boolean) -> bool {
+      return not boolean;
+    };
+
+    SECTION("identical") {
+      strategies::Booleans false_values(0.);
+      strategies::Booleans true_values(1.);
+
+      auto still_false_values = false_values.filter(negate);
+      auto still_true_values = true_values.filter(identity);
+
+      REQUIRE(still_false_values() == false_values());
+      REQUIRE(still_true_values() == true_values());
+    }
+
+    SECTION("impossible") {
+      strategies::Booleans false_values(0.);
+      strategies::Booleans true_values(1.);
+
+      auto invalid_false_values = false_values.filter(identity);
+      auto invalid_true_values = true_values.filter(negate);
+
+      REQUIRE_THROWS_AS(invalid_false_values(),
+                        strategies::OutOfTries);
+      REQUIRE_THROWS_AS(invalid_true_values(),
+                        strategies::OutOfTries);
+    }
+  }
 }
