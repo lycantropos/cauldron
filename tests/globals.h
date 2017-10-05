a204e568d9abfe0b497d64175ae0f43c14e9d4ff
Azat Ibrakov <azatibrakov@gmail.com>
2017-10-06 01:55:12 +0700

    Removing unused constants

--- tests/globals.h
+++ tests/globals.h
@@ -9,7 +9,4 @@ extern const size_t max_capacity;
 
 extern const char min_character;
 extern const char max_character;
-
-extern const size_t non_zero_characters_count;
-extern const size_t alphanumeric_characters_count;
 }
