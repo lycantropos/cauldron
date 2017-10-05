fefcba642f4f7a5893e86f151a32a4de46bff74c
Azat Ibrakov <azatibrakov@gmail.com>
2017-10-06 00:35:00 +0700

    Added 'operators' module with common operators

---
+++ tests/operators.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <algorithm>
+#include "operators.h"
+
+
+char to_upper(char character) {
+  return char(std::toupper(character));
+}
+
+
+char to_lower(char character) {
+  return char(std::tolower(character));
+}
+
+
+std::string to_upper_string(std::string string) {
+  std::string result;
+  std::transform(string.begin(),
+                 string.end(),
+                 std::back_inserter(result),
+                 to_upper);
+  return result;
+}
+
+
+std::string to_lower_string(std::string string) {
+  std::string result;
+  std::transform(string.begin(),
+                 string.end(),
+                 std::back_inserter(result),
+                 to_lower);
+  return result;
+}
---
+++ tests/operators.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+template<typename T>
+T to_even(T number) {
+  return (number / 2) * 2;
+};
+
+
+template<typename T>
+T to_odd(T number) {
+  if (number == 0 or number == 1) {
+    return 1;
+  }
+  T sign = number < 0 ? -1 : 1;
+  return (number / 2) * 2 - sign;
+};
+
+
+char to_upper(char character);
+
+
+char to_lower(char character);
+
+
+std::string to_upper_string(std::string string);
+
+
+std::string to_lower_string(std::string string);
