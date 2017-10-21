26e32142f12072390752e02fc7c9589361d3ca96
Azat Ibrakov <azatibrakov@gmail.com>
2017-10-22 00:43:55 +0700

    Added utilities for calculating sufficient set size

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -1,5 +1,4 @@
-#include <cstddef>
-#include <cmath>
+#include <catch.hpp>
 #include "utils.h"
 
 
@@ -17,4 +16,51 @@ size_t sufficient_capacity(size_t favorable_outcomes_count,
   // base equals to inverse probability
   double base = double(total_outcomes_count) / favorable_outcomes_count;
   return size_t((log(attempts_count) / log(base)));
+}
+
+
+size_t sufficient_set_size(size_t total_count, size_t attempts_count) {
+  size_t result = 1;
+  size_t total_count_exponent = total_count;
+  while (combinations(total_count,
+                      result) * attempts_count >= total_count_exponent) {
+    result++;
+    total_count_exponent *= total_count;
+  }
+  return result;
+}
+
+
+/*
+ * from https://stackoverflow.com/a/4701106/5997596
+ */
+size_t combinations(size_t total_count, size_t target_count) {
+  if (target_count > total_count) {
+    throw std::invalid_argument(
+        "Target elements count should not be greater than total count."
+    );
+  }
+  static size_t max_value = std::numeric_limits<unsigned long>::max();
+  size_t result = 1;
+  for (size_t denominator = 1;
+       denominator <= target_count;
+       ++denominator, --total_count) {
+    size_t common_divisor = gcd(result, denominator);
+    result /= common_divisor;
+    size_t step = total_count / (denominator / common_divisor);
+    if (result > max_value / step)
+      throw std::overflow_error("Overflow in combinations.");
+    result *= step;
+  }
+  return result;
+}
+
+
+size_t gcd(size_t x, size_t y) {
+  while (y != 0) {
+    size_t remainder = x % y;
+    x = y;
+    y = remainder;
+  }
+  return x;
 }
