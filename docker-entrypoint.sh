fd75743d7c2e900dde8175e041fcd8e55b9faa16
Azat Ibrakov <azatibrakov@gmail.com>
2017-10-24 13:14:34 +0700

    Added generating coverage info in 'Docker'

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,3 +7,9 @@ cmake -G "CodeBlocks - Unix Makefiles" .
 cmake --build . --target all -- -j 2
 
 ./bin/main
+
+lcov --directory . --capture --output-file coverage.info
+lcov --remove coverage.info '/usr/*' \
+                            '/opt/cauldron/tests/*' \
+                            '/root/.conan/*' \
+                            --output-file coverage.info
