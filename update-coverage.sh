e68de3531bf8ea0e0e6b4050b2a091522607acb4
Azat Ibrakov <azatibrakov@gmail.com>
2017-10-24 13:36:28 +0700

    Extracted coverage updating & removing containers into separate scripts

---
+++ update-coverage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# copying report from container
+docker container cp cauldron:/opt/cauldron/build/coverage.info coverage.info
+
+# debug info
+lcov --list coverage.info
+
+# uploading report to CodeCov
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
