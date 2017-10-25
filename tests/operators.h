406b0238bdd122d5454d783eb863a75c2b902cbf
Azat Ibrakov <azatibrakov@gmail.com>
2017-10-25 23:32:38 +0700

    Completed 'to odd/even' converters

--- tests/operators.h
+++ tests/operators.h
@@ -3,24 +3,20 @@
 
 template<typename Number>
 Number to_even(Number number) {
-  static_assert(std::is_integral<Number>::value,
+  static_assert(std::is_integral<Number>(),
                 "``Number`` should have integral type.");
-  return (number / 2) * 2;
+  return Number(number / 2) * 2;
 }
 
 
 template<typename Number>
 Number to_odd(Number number) {
-  static_assert(std::is_integral<Number>::value,
+  static_assert(std::is_integral<Number>(),
                 "``Number`` should have integral type.");
-  if (number == 0) {
-    return 1;
-  } else if (number == 1 or number == -1) {
-    return number;
-  }
   Number sign = number < 0 ? -1 : 1;
-  return (number / 2) * 2 - sign;
+  return Number(number / 2 - sign) * 2 + sign;
 }
+
 
 char to_upper(char character);
 
