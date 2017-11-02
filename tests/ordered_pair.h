6441518ec637737b5d6c7ba90530ac675a2271ec
Azat Ibrakov <azatibrakov@gmail.com>
2017-11-03 03:22:12 +0700

    Making float ordered pairs  symmetric about 0 & min distance between integer ordered pair equal to half

--- tests/ordered_pair.h
+++ tests/ordered_pair.h
@@ -18,17 +18,27 @@ static auto ordered_pair_helper(std::true_type,
   }
 
   static std::random_device random_device;
+  Number quarter = max_number / 4 + min_number / 4;
   std::uniform_int_distribution<Number> left_number_distribution(
       min_number,
-      std::max(min_number + 1, max_number / 4 + min_number / 4)
+      std::max<Number>(min_number + 1, quarter)
   );
   Number left_number = left_number_distribution(random_device);
   std::uniform_int_distribution<Number> right_number_distribution(
-      std::min(max_number - 1, max_number / 2 + min_number / 2),
+      std::min<Number>(max_number - 1, 3 * quarter),
       max_number
   );
   Number right_number = right_number_distribution(random_device);
   return std::make_tuple(left_number, right_number);
+}
+
+
+template<typename Number>
+Number abs(Number number) {
+  if (number < 0) {
+    return -number;
+  }
+  return number;
 }
 
 
@@ -39,22 +49,25 @@ static auto ordered_pair_helper(std::false_type,
   static_assert(std::is_floating_point<Number>(),
                 "``Number`` should be floating point type.");
 
-  if (min_number > 0) {
+  if (min_number >= 0) {
     throw std::invalid_argument("``min_number`` should be negative.");
   }
   if (max_number <= 0) {
     throw std::invalid_argument("``max_number`` should be positive.");
   }
 
+  Number max_modulus = std::min(abs(min_number + abs(min_number / 2)),
+                                abs(max_number - abs(max_number / 2)));
+
   static std::random_device random_device;
 
   Number min_positive_number = std::numeric_limits<Number>::min();
-  std::uniform_real_distribution<Number> left_number_distribution(
-      min_number,
-      -min_positive_number
+  std::uniform_real_distribution<Number> right_number_distribution(
+      std::max(min_positive_number, max_modulus / 2),
+      max_modulus
   );
-  Number left_number = left_number_distribution(random_device);
-  Number right_number = max_number + left_number;
+  Number right_number = right_number_distribution(random_device);
+  Number left_number = -right_number;
   return std::make_tuple(left_number, right_number);
 }
 
