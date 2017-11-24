ab37b13498d3f36dac7ee044b249af9b0b1c8617
Azat Ibrakov <azatibrakov@gmail.com>
2017-11-25 00:11:47 +0700

    Removed line break

--- update-docs.sh
+++ update-docs.sh
@@ -20,7 +20,6 @@ git remote add origin https://github.com/${TRAVIS_REPO_SLUG}.git
 git checkout -b gh-pages
 
 git add --all
-git commit -m "Deploy code docs to \"GitHub Pages\" " \
-              "from \"Travis CI\" build \"${TRAVIS_BUILD_NUMBER}\"" \
+git commit -m "Deploy code docs to \"GitHub Pages\" from \"Travis CI\" build \"${TRAVIS_BUILD_NUMBER}\"" \
            -m "Commit: ${TRAVIS_COMMIT}"
 git push --force https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
