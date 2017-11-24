cf732c5d8094f199b126256ca85cd36db66cd5b9
Azat Ibrakov <azatibrakov@gmail.com>
2017-11-24 15:19:12 +0700

    Added docs updating as 'after success' build step

---
+++ update-docs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -ex
+
+### Configure git ###
+
+# Pretend to be an user called Travis CI.
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+
+### Generating documentation ###
+
+doxygen
+
+### Pushing documentation ###
+
+cd docs/html
+git init
+git add remote origin https://github.com/${TRAVIS_REPO_SLUG}.git
+git co -b gh-pages
+
+git add --all
+git commit -m "Deploy code docs to \"GitHub Pages\" " \
+              "from \"Travis CI\" build \"${TRAVIS_BUILD_NUMBER}\"" \
+           -m "Commit: ${TRAVIS_COMMIT}"
+git push --force https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
